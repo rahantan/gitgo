bc7b60e58fb1075c5415e4ed8f3e43fa5c087e4a
rahantan <jidanrahantan@gmail.com>
2025-02-08 06:52:27 +0700

    menambahkan func-func problem solve

--- main.go
+++ main.go
@@ -1,16 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func getHello(text string) string {
-	return text
+func hitungFrekuensiPerkata() map[string]int {
+	// algoritma
+	input := "hello teman semuanya saya, cuman memberi tahu teman teman"
+
+	// Konversi teks ke huruf kecil → agar tidak membedakan huruf besar/kecil.
+	// Pisahkan kata → menggunakan strings.Fields().
+	filter := strings.Fields(strings.ToLower(input)) // Pisahkan kata
+
+	// Gunakan map → untuk menyimpan jumlah kemunculan setiap kata.
+	data := make(map[string]int)
+
+	for _, f := range filter {
+		data[f]++
+	}
+	// Cetak hasilnya → tampilkan kata dan jumlah kemunculannya.
+	return data
 }
 
-func pertambahan(angka1, angka2 int) int {
-	return angka1 + angka2
+func GanjilGenap() ([]int, []int) {
+	angka := []int{
+		5, 6, 2, 1, 6, 8, 3, 2, 1, 2, 5,
+	}
+	var ganjil, genap []int
+	for _, a := range angka {
+		if a%2 == 0 {
+			ganjil = append(ganjil, a)
+		} else {
+			genap = append(genap, a)
+		}
+	}
+	return ganjil, genap
 }
 
+func Rekursif(data *[]int, angka int) {
+	if angka < 0 {
+		return
+	}
+	Rekursif(data, angka-1)
+	*data = append(*data, angka)
+}
 func main() {
-	fmt.Println(getHello("hello world"))
-	fmt.Println(pertambahan(4, 5))
+	var data []int
+	Rekursif(&data, 4)
+	fmt.Println(data)
 }
